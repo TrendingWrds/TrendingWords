0c41bc769caaf6566c8805361272c3a74fb5b06d
austriker27 <davidalindahl@gmail.com>
2017-10-03 09:44:55 -0700

    updated homeview into functions

--- public/scripts/view/homeView.js
+++ public/scripts/view/homeView.js
@@ -1,32 +1,42 @@
 'use strict';
 
-let subredditJSON = [];
-let allSubreddits = [];
-let titlesJSON = [];
-let subredditTitles = [];
-let oneBigString = '';
-let searchMe;
+var app = app || {};
 
-// $.get('/authorize/').then(console.log);
-$.get('/api/getSubreddits/').then(results => {
-  subredditJSON = results;
-  subredditJSON.data.children.forEach(item => allSubreddits.push(item.data.display_name));
-}, err =>{
-  console.error(err);
-}).then(function(){
-  $.get(`/api/getTitles/${allSubreddits[0]}`).then(results => {
-    titlesJSON = results;
-    titlesJSON.data.children.forEach(item => {
-      subredditTitles.push(item.data.title);
+(function(module) {
+  let subredditJSON = [];
+  let allSubreddits = [];
+  let titlesJSON = [];
+  let subredditTitles = [];
+  let oneBigString = '';
+  let searchMe;
+
+  // $.get('/authorize/').then(console.log);
+  let getSubreddits = function(callback) {
+    $.get('/api/getSubreddits/')
+      .then(results => {
+        subredditJSON = results;
+        subredditJSON.data.children.forEach(item => allSubreddits.push(item.data.display_name));
+        callback();
+      }, err => {
+        console.error(err);
+      });
+  };
+
+  let getSubredditTitles = function() {
+    $.get(`/api/getTitles/${allSubreddits[0]}`).then(results => {
+      titlesJSON = results;
+      titlesJSON.data.children.forEach(item => {
+        subredditTitles.push(item.data.title);
+      });
+    }, err =>{
+      // console.error(err);
+    }).then(function(){
+      oneBigString = subredditTitles.reduce(function(acc, cur){return acc.concat(cur) + ' ';});
+      searchMe = JSON.stringify(oneBigString);
     });
-  }, err =>{
-    // console.error(err);
-  }).then(function(){
-    oneBigString = subredditTitles.reduce(function(acc, cur){return acc.concat(cur) + ' ';});
-    searchMe = JSON.stringify(oneBigString);
-  });
-});
+  };
 
+})(app);
 
 
 // $.get(`/api/getTitles/${allSubreddits[0]}`).then(results => {
