e3472d6a6482a4db4bbc4cf58c7d1982f529cc66
Marco Zangari <mjzangari@gmail.com>
2017-10-02 19:05:17 -0700

    worked through new header commands for function on razorNlp.js

--- public/scripts/controller/razorNlp.js
+++ public/scripts/controller/razorNlp.js
@@ -1,1 +1,23 @@
 'use strict';
+
+var settings = {
+  'async': true,
+  'crossDomain': true,
+  'url': 'https://api.textrazor.com/',
+  'method': 'POST',
+  'headers': {
+    'x-textrazor-key': 'fb48909b03e33ac72c8c87bf368d84dd8ac41a3ba4032f4a7a2b4586',
+    'content-type': 'application/x-www-form-urlencoded',
+    'cache-control': 'no cache',
+    'postman-token': '8b197ff1-8095-0c90-0a61-74feaf5fbc01',
+    'access-control-allow-origin': '*'
+  },
+  'data': {
+    'text': 'the lazy brown dog jumped over the brown pile of dung',
+    'extractors': 'words'
+  }
+};
+
+$.ajax(settings).done(function (response) {
+  console.log(response);
+});
