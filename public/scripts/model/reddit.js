7687b7c85f8059698be01d750aba6a84fb96cea0
austriker27 <davidalindahl@gmail.com>
2017-10-03 09:48:29 -0700

    created models and model file for reddit

---
+++ public/scripts/model/reddit.js
@@ -0,0 +1,37 @@
+
+var app = app || {};
+
+(function(module) {
+  let subredditJSON = [];
+  let allSubreddits = [];
+  let titlesJSON = [];
+  let subredditTitles = [];
+  let oneBigString = '';
+  let searchMe;
+
+  let getSubreddits = function(callback) {
+    $.get('/api/getSubreddits/')
+      .then(results => {
+        subredditJSON = results;
+        subredditJSON.data.children.forEach(item => allSubreddits.push(item.data.display_name));
+        callback();
+      }, err => {
+        console.error(err);
+      });
+  };
+
+  let getSubredditTitles = function() {
+    $.get(`/api/getTitles/${allSubreddits[0]}`).then(results => {
+      titlesJSON = results;
+      titlesJSON.data.children.forEach(item => {
+        subredditTitles.push(item.data.title);
+      });
+    }, err =>{
+      // console.error(err);
+    }).then(function(){
+      oneBigString = subredditTitles.reduce(function(acc, cur){return acc.concat(cur) + ' ';});
+      searchMe = JSON.stringify(oneBigString);
+    });
+  };
+
+})(app);
