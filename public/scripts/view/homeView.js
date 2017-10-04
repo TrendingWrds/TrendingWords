1d6e3d041e6429d258dd12c5832e7d7d1ccafb90
Marco Zangari <mjzangari@gmail.com>
2017-10-04 11:56:57 -0700

    adding the handlebars template; trying to manipulate the object list from reddit

--- public/scripts/view/homeView.js
+++ public/scripts/view/homeView.js
@@ -5,12 +5,30 @@ var app = app || {};
 (function(module) {
 
   let renderSubreddits = function () {
-    let allSubredditsObjects = {};
     // TODO: turn above into an array of objects of all the subjects so we can run handlebars on it.
-
+    let newArray = [];
+    for (var i = 0; i < app.allSubreddits.length; i++) {
+      newArray.push({
+        key: 'subredditName',
+        value: [i]
+      });
+    }
     let render = Handlebars.compile($('#subredditName-template').text());
-    $('#subredditListAnchor').append(app.allSubreddits.map(render));
+    $('#subredditListAnchor').append(newArray.map(render));
   };
 
   module.renderSubreddits = renderSubreddits;
 })(app);
+
+
+
+
+// let newArray = app.allSubreddits.slice();
+// let keys = newArray.shift();
+// newArray = newArray.map(function(e) {
+//   let obj = {};
+//   keys.forEach(function(key,i){
+//     obj[key] = e[i];
+//   });
+//   console.log('hello');
+  // return obj;
