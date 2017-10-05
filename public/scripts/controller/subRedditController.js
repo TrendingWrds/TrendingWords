0ecd59e01be8e8ecfbf78a135afc6b7cd3fb430d
Marco Zangari <mjzangari@gmail.com>
2017-10-05 10:00:24 -0700

    changed .bashrc to reflect PORT and DATABASE; also removed constring on server.js from template literal set as an environment variable

--- public/scripts/controller/subRedditController.js
+++ public/scripts/controller/subRedditController.js
@@ -7,5 +7,8 @@ var app = app || {};
   // this runs the get subreddits function which passes the functions to get the subreddit titles and the function that sets up a route to send them to the server side via callbacks to avoid async
   app.getSubreddits(app.getSubredditTitles, app.fillTableWithSubredditNames, app.sendSubredditTitles, app.renderSubreddits);
 
+  $('li.subreddit').on('click', function(){
+    console.log( $(this).text() );
+  });
 
 })(app);
