f244018dece13b88f990d87496c5a8ea45eab561
austriker27 <davidalindahl@gmail.com>
2017-10-05 11:30:31 -0700

    completed live search bar fixing the bug that didnt display all the results

--- public/scripts/view/searchbarView.js
+++ public/scripts/view/searchbarView.js
@@ -6,22 +6,10 @@ var app = app || {};
   let $searchbar = $('.searchbar');
   $searchbar.on('keyup', function() {
     let searchString = $searchbar.val().trim().toUpperCase();
-    console.log(searchString);
     let filteredIn = $('.subredditNameLi').toArray().filter(function(li) {
       return li.id.includes(searchString);
     });
-    console.log(filteredIn);
-    let filteredOut = $('.subredditNameLi').toArray().filter(function(li){
-      return !li.id.includes(searchString);
-    });
-    console.log(filteredOut);
+    $('.subredditNameLi').hide();
     $(filteredIn).show();
-    $(filteredIn).each((idx, item) => {
-      $(item).next().show();
-    });
-    $(filteredOut).hide();
-    $(filteredOut).each((idex, item) => {
-      $(item).next().hide();
-    });
   });
 })(app);
