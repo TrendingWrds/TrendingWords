e20bbcf1cd400eea5674aebf440c159a35b23fa6
austriker27 <davidalindahl@gmail.com>
2017-10-05 10:44:31 -0700

    tweaking handlebars to prepare for searchbar

---
+++ public/scripts/view/searchbarView.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var app = app || {};
+
+(function(module) {
+  let $searchbar = $('.searchbar');
+  $searchbar.on('keyup', function() {
+    let searchString = $searchbar.val().trim().toUpperCase();
+    console.log(searchString);
+    let filteredIn = $('.subredditNameLi').toArray().toUpperCase().filter(function(li) {
+      return li.id.includes(searchString);
+    });
+    console.log(filteredIn);
+    let filteredOut = $('.subredditNameLi').toArray().toUpperCase().filter(function(li){
+      return !li.id.includes(searchString);
+    });
+    console.log(filteredOut);
+    $(filteredIn).show();
+    $(filteredIn).each((idx, item) => {
+      $(item).next().show();
+    });
+    $(filteredOut).hide();
+    $(filteredOut).each((idex, item) => {
+      $(item).next().hide();
+    });
+  });
+})(app);
