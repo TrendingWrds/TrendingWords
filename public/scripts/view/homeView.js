e0d140f89f4bf757e1615ea6df5015fa5f71fabc
Michael Shinners <mishinners@icloud.com>
2017-10-05 13:49:57 -0700

    refactored linking code  pto operate our key function

--- public/scripts/view/homeView.js
+++ public/scripts/view/homeView.js
@@ -6,16 +6,16 @@ var app = app || {};
 
   let renderSubreddits = function (callback) {
 
-  Handlebars.registerHelper('makeAllCaps', function(text) {
-    return text.toUpperCase();
-  });
-    
+    Handlebars.registerHelper('makeAllCaps', function(text) {
+      return text.toUpperCase();
+    });
+
     let render = Handlebars.compile($('#subredditName-template').html());
     $('#subredditListAnchor').append(render({keys: app.allSubreddits}));
     callback && callback();
   };
 
-  let addListeners = function (){$('li.subreddit').on('click', function(){
+  let addListeners = function (){$('.subredditName').on('click', function(){
     console.log( $(this) );
     app.subredditDestination = this.id;
     app.getSubredditTitles(app.sendSubredditTitles, app.subredditDestination);
