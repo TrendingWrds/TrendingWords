3de63c6980c3fe10d7047600bab89a6adb435dea
Michael Shinners <mishinners@icloud.com>
2017-10-05 16:07:07 -0700

    revised Pages functionality and added Dev Photos

--- public/scripts/view/homeView.js
+++ public/scripts/view/homeView.js
@@ -6,6 +6,10 @@ var app = app || {};
   let showMain = function () {
     $('#main').show();
     $('#aboutUs').hide();
+    $('.wordCloudResult').empty();
+    $('.subreddit-lists').show();
+    $('.subreddit-search').show();
+    $('.directions').show();
   };
 
   let renderSubreddits = function (callback) {
@@ -24,7 +28,7 @@ var app = app || {};
     app.subredditDestination = this.id;
     app.getSubredditTitles(app.sendSubredditTitles, app.subredditDestination, app.initResultsPage);
   });};
-  
+
   module.showMain = showMain;
   module.renderSubreddits = renderSubreddits;
   module.addListeners = addListeners;
--- public/scripts/view/resultsView.js
+++ public/scripts/view/resultsView.js
@@ -3,11 +3,8 @@
 var app = app || {};
 
 (function(module) {
-  // hide everything that we dont want to see
-    // hiding all non-selected subreddits
-  // show resulting image, and link to selected subreddit
-    // home and about buttons showing
   let initResultsPage = function() {
+    $('.wordCloudResult').empty();
     $('.subreddit-lists').hide();
     $('.subreddit-search').hide();
     $('.directions').hide();
